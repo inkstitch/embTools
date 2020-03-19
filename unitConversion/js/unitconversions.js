ab0c39033058b7f8d1d4ac9bdab7559adbd82207
wwderw <24878362+wwderw@users.noreply.github.com>
2020-03-19 06:30:56 -0500

    Add files via upload

---
+++ unitConversion/js/unitconversions.js
@@ -0,0 +1,79 @@
+
+// conversions
+
+  // Inches to MM
+
+var inch = document.querySelector("#inputIN");
+var mm = document.querySelector("#inputMM");
+
+inch.addEventListener('input', function(){
+  let input = parseFloat(document.getElementById("inputIN").value);
+
+  let answer = document.getElementById("inputMM");
+  answer.value = input * 25.4;
+})
+
+mm.addEventListener("input", function(){
+    let input = parseFloat(document.getElementById("inputMM").value);
+
+    let answer = document.getElementById("inputIN");
+
+    answer.value = input / 25.4;
+})
+
+  // Inches to CM
+
+var inch2 = document.querySelector("#inputin");
+var cm = document.querySelector("#inputCM");
+
+inch2.addEventListener('input', function(){
+   let input = parseFloat(document.getElementById("inputin").value);
+
+   let answer = document.getElementById("inputCM");
+   answer.value = input / 0.39370;
+
+})
+
+cm.addEventListener('input', function(){
+
+   let input = parseFloat(document.getElementById("inputCM").value);
+
+   let answer = document.getElementById("inputin");
+
+   answer.value = input * 0.39370;
+
+})
+
+// Pixels to MM
+
+var px = document.querySelector("#inputPX");
+var mm2 = document.querySelector("#inputMm");
+
+px.addEventListener('input', function(){
+  let input = parseFloat(document.getElementById("inputPX").value);
+  let answer = document.getElementById("inputMm");
+  answer.value = input * 0.2645833333;
+})
+
+mm2.addEventListener('input', function(){
+  let input = parseFloat(document.getElementById("inputMm").value);
+  let answer = document.getElementById("inputPX");
+  answer.value = input / 0.2645833333;
+})
+
+// PT to IN
+
+var pt = document.querySelector("#inputPT");
+var inPT = document.querySelector("#inputInches");
+
+pt.addEventListener('input', function(){
+  let input = parseFloat(document.getElementById("inputPT").value);
+  let answer = document.getElementById("inputInches");
+  answer.value = input / 72;
+})
+
+inPT.addEventListener('input', function(){
+let input = parseFloat(document.getElementById("inputInches").value);
+let answer = document.getElementById("inputPT");
+answer.value = input * 72;
+})
